49fe034636c7c75e59cfd1b1e4f9ff03e2ac58b3
marcin-bojar <marcin.bojar@interia.pl>
2020-09-30 21:00:50 +0200

    Added dynamic Wordpress menus into the header and footer

--- app/public/wp-content/themes/amazing-college-theme/functions.php
+++ app/public/wp-content/themes/amazing-college-theme/functions.php
@@ -1,16 +1,20 @@
-<?php 
-    function university_files() {
-        wp_enqueue_script('main-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
-        wp_enqueue_style('google-font', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
-        wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
-        wp_enqueue_style('university_main_styles', get_stylesheet_uri());
-    }
+<?php
+function university_files()
+{
+    wp_enqueue_script('main-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
+    wp_enqueue_style('google-font', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+    wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+    wp_enqueue_style('university_main_styles', get_stylesheet_uri());
+}
 
-    add_action('wp_enqueue_scripts', 'university_files');
+add_action('wp_enqueue_scripts', 'university_files');
 
-    function university_features() {
-        add_theme_support('title-tag');
-    }
+function university_features()
+{
+    register_nav_menu('headerMenuLocation', 'Header Menu Location');
+    register_nav_menu('footerMenuLocation1', 'Footer Menu Location One');
+    register_nav_menu('footerMenuLocation2', 'Footer Menu Location Two');
+    add_theme_support('title-tag');
+}
 
-    add_action('after_setup_theme', 'university_features');
-?>
+add_action('after_setup_theme', 'university_features');
